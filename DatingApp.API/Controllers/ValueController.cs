a99155b243cebda1f9e7f2e6e053bd3f37b513d3
mohamed salah elkholy <67386713+MoohamedSalah@users.noreply.github.com>
2021-11-16 23:48:26 +0200

    inital commit
    Signed-off-by: mohamed salah elkholy <67386713+MoohamedSalah@users.noreply.github.com>

---
+++ DatingApp.API/Controllers/ValueController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DatingApp.API.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace DatingApp.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValueController : ControllerBase
+    {
+        private readonly DataContext _context;
+        public ValueController(DataContext context)
+        {
+            _context = context;
+
+        }
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            return Ok(await _context.Values.ToListAsync());
+        }
+        [HttpGet("{Id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            return Ok(await _context.Values.FirstOrDefaultAsync(s => s.Id == id));
+        }
+
+    }
+}
